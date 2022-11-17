349b31d257aeac6c1050139e84c2eea8010fb08f
Ron <admin@darkmagick.finance>
2022-11-17 12:39:03 -0500

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "DarkMagick",
+    subtitle: "HTML and Boostrap",
+    description:
+      "Cryptocurrency Website. Built using HTML and Bootstrap, no React code is used. This showcases my ability to create a one page website without React."
+      ,
+    image: "./project-1.png",
+    link: "https://darkmagick.netlify.com",
+  },
+  {
+    title: "Rick & Morty Fanpage",
+    subtitle: "React, Material UI & AXIOS",
+    description:
+      "Ricky and Morty episode fan page created as a school project. Showcasing my skillset with React and retreiving API data. I used Material UI for the design framework.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "NYSE Data Analysis",
+    subtitle: "Data analysis project. ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Want to know more about my work history? Check out my resume",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Resume",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "MySQL",
+  "Express",
+  "React",
+  "Node.js",
+  "JavaScript/ES6",
+  "Material UI",
+  "REST API",
+  "Data Structures",
+  "Algorithms",
+  "Agile Development",
+  "CSS / SCSS / Tailwind",
+  "Responsive Web Design"
+];
